be50bf17bee0f4479181a680f28f5b0d465a7469
behei-vonage <mykhailo.behei@gmail.com>
2025-10-18 14:20:03 -0500

    fix

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -53,7 +53,7 @@ const Contact = (): ReactElement => {
               </div>
               <div className="contact-item">
                 <strong>Location:</strong>
-                <span>West Bend, Wisconsi</span>
+                <span>West Bend, Wisconsin</span>
               </div>
               <div className="contact-item">
                 <strong>GitHub:</strong>
